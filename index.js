9a5445067af86076d075e379cb282c2cae958e8a
Manikanta <98543225+Manikanta12n@users.noreply.github.com>
2023-02-20 21:39:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function seq(){
 }
 
 function playSound(name) {
-    var audio = new Audio("Simon/sounds/" + name + ".mp3");
+    var audio = new Audio("Manikanta12n/Simon/sounds/" + name + ".mp3");
     audio.play();
 }
 
